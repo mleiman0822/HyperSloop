a5798bb02a9926b1062b4c188d5887f02e28d08f
BNGmleiman <77414414+BNGmleiman@users.noreply.github.com>
2021-04-22 11:05:37 -0500

    Quick commit

---
+++ HyperSloopApp/Data/HyperSloopContext.cs
@@ -0,0 +1,14 @@
+﻿using HyperSloopApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HyperSloopApp.Data
+{
+    public class HyperSloopContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ HyperSloopApp/Models/UserCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HyperSloopApp.Models
+{
+    public class UserCard
+    {
+        public int KeyCardId { get; set; }
+        public int UserId { get; set; }
+        public string Email { get; set; }
+
+        //Navigation property that creates a one to many relationship. User has 1 or more events.
+        public ICollection<Events> Events { get; set; }
+    }
+}
